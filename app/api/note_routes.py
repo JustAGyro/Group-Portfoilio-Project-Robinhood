fe3705f2091d5feec54d24ec9425b69851156dde
Kamran <kamran.zq@gmail.com>
2023-06-21 18:38:33 -0500

    edited note routes

--- app/api/note_routes.py
+++ app/api/note_routes.py
@@ -3,3 +3,59 @@ from flask_login import login_required
 from app.models import NoteSymbol, Note
 
 note_routes = Blueprint('notes', __name__)
+
+
+# GET /api/notes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# GET /api/notes/:noteId
+
+
+
+
+
+
+
+
+
+
+
+
+# POST /api/notes/new
+
+
+
+
+
+
+
+
+
+
+
+# DELETE /api/notes/:noteId/delete
+
+
+
+
+
+
+
+
+
+
+# PUT /api/notes/:noteId
