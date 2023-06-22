9f44942a33e5413749bdf98c1bb1e02a1d850eab
ahmad-shaukat <ahmadshaukat@yahoo.com>
2023-06-21 20:53:42 -0400

    imported db

--- app/api/note_routes.py
+++ app/api/note_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.models import NoteSymbol, Note
+from app.models import NoteSymbol, Note, db
 
 
 note_routes = Blueprint('notes', __name__)
