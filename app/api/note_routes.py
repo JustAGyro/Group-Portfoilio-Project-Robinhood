7413ccf451d942ac657e3a7ac75bcf09ca82fb68
ahmad-shaukat <ahmadshaukat@yahoo.com>
2023-06-21 20:48:39 -0400

    added routes for post and delete note

--- app/api/note_routes.py
+++ app/api/note_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app.models import NoteSymbol, Note
+
 
 note_routes = Blueprint('notes', __name__)
 
@@ -36,6 +37,25 @@ note_routes = Blueprint('notes', __name__)
 
 
 # POST /api/notes/new
+@note_routes('/new', methods=['POST'])
+@login_required
+def add_note():
+    # data is destructured
+    subject = 'Note Subject'
+    userId = current_user.id
+    entry = 'this is the form entry'
+    new_note = Note ( 
+        userId = userId,
+        subject = subject,
+        entry = entry
+    )
+    db.session.add(new_note)
+    db.session.commit()
+    return 'Successfully Added'
+
+
+
+
 
 
 
@@ -49,6 +69,16 @@ note_routes = Blueprint('notes', __name__)
 
 # DELETE /api/notes/:noteId/delete
 
+@note_routes('/<int:noteId>/delete', methods=['Delete'])
+@login_required
+def delete_note(noteId):
+    note = Note.query.get(noteId)
+    if not note: 
+        return ('note not found')
+    if current_user.id == note.userId:
+        db.session.delete(note)
+        db.session.commit()
+        return 'Successfully deleted note'
 
 
 
