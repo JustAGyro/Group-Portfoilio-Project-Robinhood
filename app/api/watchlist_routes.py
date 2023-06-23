5738827e05f01191ff3941e95f60d901039c8ff0
Kamran <kamran.zq@gmail.com>
2023-06-22 22:48:20 -0500

    cleaned up code

--- app/api/watchlist_routes.py
+++ app/api/watchlist_routes.py
@@ -5,37 +5,37 @@ from app.forms.watchlist_form import WatchListForm
 
 watchlist_routes = Blueprint('watchlists', __name__)
 
-#Test Route
+# Test Route
 @watchlist_routes.route('/test')
 def watchlist_test():
     in_route = "In Route :)"
     return jsonify(in_route)
 
-#GET all watchlists for the current user
+# GET /api/watchlists/current
+# GET all watchlists for the current user
 @watchlist_routes.route('/current')
 @login_required
 def watchlist_current():
-    """
-    Query for all watchlists associated with the current user
-    """
+
     user_id = current_user.id
     watchlists = WatchList.query.join(SymbolList).filter(WatchList.userId == user_id).all()
     list = [watchlist.to_dict() for watchlist in watchlists]
 
     return jsonify(list)
 
-#GET watchlist by watchlist_id
+# GET /api/watchlists/<id>
+# GET watchlist by watchlist_id
 @watchlist_routes.route('/<id>')
 @login_required
 def watchlist_one(id):
     watchlist = WatchList.query.get(1)
     return jsonify(watchlist.to_dict())
 
-#POST watchlist /api/watchlists/new
+# POST /api/watchlists/new
+# POST a new watchlist
 @watchlist_routes.route('/new', methods=['POST'])
 @login_required
 def add_watchlist():
-    #data is destructured
     form = WatchListForm()
 
     new_watchlist = WatchList(
@@ -47,8 +47,8 @@ def add_watchlist():
 
     return jsonify(new_watchlist.to_dict())
 
-#DELETE watchlist /api/watchlists/<id>/delete
-
+# DELETE /api/watchlists/<id>/delete
+# DELETE a watchlist
 @watchlist_routes.route('/<id>/delete', methods=['DELETE'])
 @login_required
 def delete_watchlist(id):
@@ -60,11 +60,13 @@ def delete_watchlist(id):
         db.session.commit()
         return 'Successfully Deleted Watchlist'
 
-#PUT watchlist /api/watchlists/<id>/edit
+# PUT /api/watchlists/<id>/edit
+# PUT edit a watchlist
 @watchlist_routes.route('/<id>/edit', methods=['PUT'])
 @login_required
 def edit_watchlist(id):
     watchlist = WatchList.query.get(id)
+
     if watchlist:
         form = WatchListForm()
         form.name.data=watchlist.name
