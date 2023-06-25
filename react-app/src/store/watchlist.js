96eb517d1bace176136e5fe87cd9adcf3ddb2723
Kamran <kamran.zq@gmail.com>
2023-06-25 16:53:37 -0500

    added watchlist reducer

---
+++ react-app/src/store/watchlist.js
@@ -0,0 +1,97 @@
+const GET_WATCHLIST = 'watchlist/GET_WATCHLIST';
+const ADD_WATCHLIST = 'watchlist/ADD_WATCHLIST';
+const DELETE_WATCHLIST = 'watchlist/DELETE_WATCHLIST';
+
+export const getAllWatchlists = (watchlists) => {
+  return {
+    type: GET_WATCHLIST,
+    payload: watchlists,
+  };
+};
+
+export const addWatchlists = (watchlist) => {
+  return {
+    type: ADD_WATCHLIST,
+    payload: watchlist,
+  };
+};
+
+export const deleteWatchlist = (watchlist) => {
+  return {
+    type: DELETE_WATCHLIST,
+    payload: watchlist,
+  };
+};
+
+export const getAllWatchlistsThunk = () => async (dispatch) => {
+  const response = await fetch('/api/watchlists/current');
+
+  if (response.ok) {
+    const watchlists = await response.json();
+    await dispatch(getAllWatchlists(watchlists));
+    return watchlists;
+  }
+};
+
+export const getOneWatchlistThunk = (id) => async (dispatch) => {
+  const response = await fetch(`api/watchlists/${id}`);
+
+  if (response.ok) {
+    const data = await response.json();
+    await dispatch(addWatchlists(data));
+    return data;
+  }
+};
+
+export const createWatchlistThunk = (watchlist) => async (dispatch) => {
+  const response = await fetch('/api/watchlists/new', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(watchlist),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(addWatchlists(data));
+    return data;
+  }
+};
+
+export const deleteWatchlistThunk = (watchlist) => async (dispatch) => {
+  const reposne = await fetch(`/api/watchlists/delete/${watchlist.id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(watchlist),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(deleteWatchlist(watchlist.id));
+  }
+};
+
+export default function watchlistsReducer(state = {}, action) {
+  let newState = {};
+  switch (action.type) {
+    case GET_WATCHLIST:
+      newState = { ...state };
+      action.payload.forEach((watchlist) => {
+        newState[watchlist.id] = watchlist;
+      });
+      return newState;
+    case ADD_WATCHLIST:
+      newState = { ...state };
+      newState[action.payload.id] = action.payload;
+      return newState;
+    case DELETE_WATCHLIST:
+      newState = { ...state };
+      delete newState[action.payload];
+      return newState;
+    default:
+      return state;
+  }
+}
