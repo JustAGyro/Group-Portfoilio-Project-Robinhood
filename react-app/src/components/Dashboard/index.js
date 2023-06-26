c4b361ad06f501c0f15c1ab1510e9b8214f35c6b
Kamran <kamran.zq@gmail.com>
2023-06-26 17:05:40 -0500

    dashboard

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -14,20 +14,6 @@ export default function ShowDashboard() {
           <div class="left-gutter"></div>
           <div class="dashboard">
             <h1>this is the dashboard. .</h1>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
-            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
           </div>
         </div>
       </div>
