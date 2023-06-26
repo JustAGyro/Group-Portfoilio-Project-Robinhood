b52d2bcf596c9603434436e9004dd0a7f873f4da
Kamran <kamran.zq@gmail.com>
2023-06-25 19:22:07 -0500

    styling dashboard

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import './Dashboard.css';
 
 export default function ShowDashboard() {
   return (
     <>
-      <div>The start of the dashboard :D </div>
+      <div class="container">
+        <div class="navbar"></div>
+        <div class="content">
+          <div class="stock-watchlist">
+            <h1>this is the watchlist</h1>
+          </div>
+          <div class="right-gutter"></div>
+          <div class="left-gutter"></div>
+          <div class="dashboard">
+            <h1>this is the dashboard. .</h1>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+            <img src="https://image.cnbcfm.com/api/v1/image/107230946-1682453373313-aaa_img_100_99_112_1957665_1000-1682453283939.jpg?v=1682453960"></img>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -4,21 +4,25 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
-	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
-	);
+  return (
+    <div class="bar">
+      <ul>
+        <li>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+        </li>
+        {isLoaded && (
+          <li>
+            <ProfileButton user={sessionUser} />
+          </li>
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default Navigation;
