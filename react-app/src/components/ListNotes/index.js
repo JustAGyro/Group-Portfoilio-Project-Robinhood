a437ec15efd483fb5a830d784ea69d9f6d596826
Aaron Zamanzadeh <Zaman.aaron2@gmail.com>
2023-06-27 16:25:36 -0700

    fixed notes list

--- react-app/src/components/ListNotes/index.js
+++ react-app/src/components/ListNotes/index.js
@@ -1,14 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { getAllNotes, getNoteByIdThunk, updateNoteThunk } from "../../store/notes"
 import { useHistory, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function ListNotes() {
+    const dispatch = useDispatch();
+    let notes = useSelector(state => state?.notes)
+    notes = Object.values(notes).map(obj => {
+        return Object.values(obj)
+    })
+    notes.forEach(element => {
+        element[3] = Object.values(element[3]).map(element => {
+            return Object.values(element)
+        })
+    });
 
-    const response = fetch(`/api/notes/mine`).then(res => res.json()).then(res => console.log(res))
 
+    useEffect(() => {
+        dispatch(getAllNotes())
+    },[])
     return (
         <div>
-            yesss
+            {notes.map(arr => {
+                return (
+                    <div>
+                        {`${arr}`}
+                    </div>
+                )
+            })}
         </div>
     )
 }
