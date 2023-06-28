c66e21e4f1f71e160c5b576ce2355a7459c16eff
Kamran <kamran.zq@gmail.com>
2023-06-27 19:29:24 -0500

    added stock historical route

--- app/api/stock_routes.py
+++ app/api/stock_routes.py
@@ -6,12 +6,14 @@ from urllib.request import urlopen
 
 stock_routes = Blueprint('stocks', __name__)
 
-# Financial Ratios Route
-@stock_routes.route('/financial_ratios/<symbol>')
+# Historical Daily Price for a specific stock
+@stock_routes.route('/historical_daily/<symbol>')
 @login_required
-def financial_ratios(symbol):
-    url =f"https://financialmodelingprep.com/api/v3/key-metrics-ttm/{symbol}?limit=1&apikey=c4af6834a77de852f5ef970e0b5dd457"
+def historical_daily(symbol):
+    url =f"https://financialmodelingprep.com/api/v3/historical-price-full/{symbol}?serietype=line&timeseries=365&apikey=c4af6834a77de852f5ef970e0b5dd457"
     response = urlopen(url, cafile=certifi.where())
     data = response.read().decode("utf-8")
-    print(data)
+    # Todo !!
+    # Parse the data and only take what we need for this specific route
+    # Change return to only return the new parsed data.
     return json.loads(data)
---
+++ stock_api_routes.md
@@ -0,0 +1,22 @@
+# Stock API Routes
+
+## Stock Detail Page
+
+- Stock Price Real Time
+- Company Quote
+- Historical Price
+- Company Profile
+- Single Stock News
+
+## Stock Search
+
+- Stock search API
+
+## Dashboard / Portfolio
+
+- Stock Price
+- Stock news (all) / General news / sector news etc...
+
+## Watchlist
+
+- Stock Price Real Time
