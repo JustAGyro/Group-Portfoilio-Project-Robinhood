1593dd3cfa0b7f7bca603ba7a767fd583d22333b
Kamran <kamran.zq@gmail.com>
2023-07-02 18:46:48 -0500

    got chart to look fine

---
+++ react-app/src/components/DetailsGraph/index.js
@@ -0,0 +1,125 @@
+import {
+  createChart,
+  ColorType,
+  PriceScaleMode,
+  CrosshairMode,
+} from 'lightweight-charts';
+import React, { useEffect, useRef, useState } from 'react';
+import './DetailsGraph.css';
+
+export default function DetailGraph(props) {
+  let {
+    data,
+    colors: {
+      backgroundColor = 'black',
+      lineColor = 'green',
+      textColor = 'white',
+      areaTopColor = 'black',
+      areaBottomColor = 'black',
+    } = {},
+  } = props;
+
+  const [currentPrice, setCurrentPrice] = useState(null);
+
+  const chartContainerRef = useRef();
+  const seriesRef = useRef(null);
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (chartRef.current) {
+        chartRef.current.applyOptions({
+          width: chartContainerRef.current.clientWidth,
+        });
+      }
+    };
+
+    chartRef.current = createChart(chartContainerRef.current, {
+      layout: {
+        background: { type: ColorType.Solid, color: backgroundColor },
+        textColor,
+      },
+      width: chartContainerRef.current.clientWidth,
+      height: 340,
+      rightPriceScale: {
+        visible: false,
+      },
+      timeScale: {
+        visible: false,
+      },
+      grid: {
+        vertLines: {
+          color: 'rgba(0, 0, 0, 0)',
+        },
+        horzLines: {
+          color: 'rgba(0, 0, 0, 0)',
+        },
+      },
+      crosshair: {
+        mode: CrosshairMode.Normal,
+        vertLine: {
+          style: 0,
+          visible: true,
+          width: 1,
+          color: 'white',
+        },
+        horzLine: {
+          visible: false,
+        },
+        // Add a listener to get the price at crosshair position
+        priceFormatter: (value) => {
+          setCurrentPrice(value);
+        },
+      },
+    });
+
+    chartRef.current.applyOptions({
+      priceScale: { mode: PriceScaleMode.Logarithmic },
+    });
+    chartRef.current.timeScale().fitContent();
+    seriesRef.current = chartRef.current.addAreaSeries({
+      lineColor,
+      topColor: areaTopColor,
+      bottomColor: areaBottomColor,
+      priceLineVisible: false,
+    });
+    seriesRef.current.setData(data);
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      if (chartRef.current) {
+        chartRef.current.remove();
+      }
+    };
+  }, [
+    data,
+    backgroundColor,
+    lineColor,
+    textColor,
+    areaTopColor,
+    areaBottomColor,
+  ]);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      chartRef.current.applyOptions({
+        handleScroll: false,
+        handleScale: false,
+        handleMouseWheel: false,
+      });
+    }
+  }, []);
+
+  console.log(currentPrice);
+
+  return (
+    <div className="chart-container-div">
+      <div ref={chartContainerRef} />
+      <div className="chart-child">
+        Price: {currentPrice != null ? currentPrice.toFixed(2) : '-'}
+      </div>
+    </div>
+  );
+}
--- react-app/src/components/StockDetail/index.js
+++ react-app/src/components/StockDetail/index.js
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './StockDetail.css';
+import DetailGraph from '../DetailsGraph';
 
 export default function ShowStockDetail() {
   const { symbol } = useParams();
   const [stockQuote, setStockQuote] = useState({});
   const [stockInfo, setStockInfo] = useState({});
   const [stockNews, setStockNews] = useState([]);
+
+  const [initialData, setInitialData] = useState([
+    { time: '2018-12-22', value: 32.51 },
+    { time: '2018-12-23', value: 31.11 },
+    { time: '2018-12-24', value: 27.02 },
+    { time: '2018-12-25', value: 27.32 },
+    { time: '2018-12-26', value: 25.17 },
+    { time: '2018-12-27', value: 28.89 },
+    { time: '2018-12-28', value: 25.46 },
+    { time: '2018-12-29', value: 23.92 },
+    { time: '2018-12-30', value: 22.68 },
+    { time: '2018-12-31', value: 22.67 },
+    { time: '2019-01-01', value: 22.0 },
+    { time: '2019-01-02', value: 23.0 },
+    { time: '2019-01-03', value: 24.0 },
+    { time: '2019-01-04', value: 26.0 },
+    { time: '2019-01-05', value: 23.0 },
+    { time: '2019-01-06', value: 21.0 },
+    { time: '2019-01-07', value: 29.0 },
+    { time: '2019-01-08', value: 28.0 },
+    { time: '2019-01-09', value: 24.0 },
+    { time: '2019-01-10', value: 25.0 },
+  ]);
 
   const fetchQuote = (symbol) => {
     if (symbol) {
@@ -63,9 +87,10 @@ export default function ShowStockDetail() {
         <div class="details">
           <div class="details-name-price">
             <h1>{symbol}</h1>
+            <p>price $</p>
           </div>
           <div class="details-graph">
-            <p>graph!</p>
+            <DetailGraph data={initialData} />
           </div>
           <div class="details-about">
             <div class="about-title">
@@ -137,6 +162,25 @@ export default function ShowStockDetail() {
           <div class="details-news">
             <h2>News</h2>
           </div>
+          {stockNews.map((article, index) => (
+            <a className="sd-link" href={article.url}>
+              <div className="news-card" key={index}>
+                <div className="article-text">
+                  <h4>
+                    {article.site} - {article.publishedDate}
+                  </h4>
+                  <h2>{article.title}</h2>
+                  <p>{article.text}</p>
+                </div>
+                <div className="article-img">
+                  <img
+                    className="article-img-thumbnail"
+                    src={article.image}
+                  ></img>
+                </div>
+              </div>
+            </a>
+          ))}
         </div>
         <div class="actions">
           <div class="action-buttons">
