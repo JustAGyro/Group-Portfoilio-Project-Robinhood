f66b3e5b4331f2a6c57a7dfc3496a0ef38345f07
Kamran <kamran.zq@gmail.com>
2023-07-12 22:08:09 -0500

    css for movers done

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Dashboard.css';
-import Movers from '../Movers'
+import Movers from '../Movers';
 import { NewsBox } from '../News';
 import Portfolio from '../Portfolio';
 import WatchLists from '../WatchLists';
@@ -8,42 +8,37 @@ import { NewTransaction } from '../Transactions';
 import ListNotes from '../ListNotes';
 export default function ShowDashboard() {
   return (
-      <div class="dash-container">
-        <div class="navbar"></div>
-        <div class="content">
-          <div class='fixed-bar'>
-            <div class="transaction-Form">
-              <NewTransaction />
-            </div>
-            <div class="stock-watchlist">
-              <WatchLists />
-            </div>
-            <div class="note-list">
-              <ListNotes />
-            </div>
-          </div>
-
-          <div class="right-gutter"></div>
-          <div class="left-gutter"></div>
-          <div class="dashboard">
-            <h1>Dashboard</h1>
-            <div class="dash-portfolio">
-              <Portfolio />
-            </div>
-
-
-            <div class="dash-movers">
-                <Movers/>
-            </div>
-            <div class="dash-news">
-              <div className="dash-news">
-                <div >
-                  <NewsBox />
-                </div>
-              </div>
+    <div class="dash-container">
+      <div class="left-gutter"></div>
+      <div class="dashboard">
+        <h1 classname="dashboard-h1">Dashboard</h1>
+        <div class="dash-portfolio">
+          <Portfolio />
+        </div>
+        <div class="dash-movers">
+          <Movers />
+        </div>
+        <div class="dash-news">
+          <div className="dash-news">
+            <div>
+              <NewsBox />
             </div>
           </div>
         </div>
       </div>
+      <div class="fixed-bar">
+        <div class="transaction-Form">
+          <NewTransaction />
+        </div>
+        <div class="stock-watchlist">
+          <WatchLists />
+        </div>
+        <div class="note-list">
+          <ListNotes />
+        </div>
+      </div>
+
+      <div class="right-gutter"></div>
+    </div>
   );
 }
--- react-app/src/components/Movers/index.js
+++ react-app/src/components/Movers/index.js
@@ -1,58 +1,34 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getGainers } from "../../store/stocks";
-import './Movers.css'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGainers } from '../../store/stocks';
+import './Movers.css';
 
 export default function Movers() {
-    const dispatch = useDispatch();
-    const [movers, setMovers] = useState([])
+  const dispatch = useDispatch();
+  const [movers, setMovers] = useState([]);
 
-    useEffect(() => {
-        const parseMovers = async () => {
-            const movers = await dispatch(getGainers())
-            // console.log("movers", movers)
-            setMovers(movers);
-            // console.log(movers)
-            return movers
-        }
-        parseMovers()
-    }, [])
-    let parsed = []
-    if(movers) {
-        parsed = movers.map(ele => {
-            return (
-                <div class="movers-card">
-                    <div class="movers-card-change">
-                    change: {ele.change}
-                    </div>
-                    <div class="movers-card-changepercentage">
-                    changesPercentage: {ele.changesPercentage}
-                    </div>
-                    <div class="movers-card-name">
-                    name: {ele.name}
-                    </div>
-                    <div class="movers-card-price">
-                    price: {ele.price}
-                    </div>
-                    <div class="movers-card-symbol">
-                    symbol: {ele.symbol}
-                    </div>
-                    <div>
+  useEffect(() => {
+    const parseMovers = async () => {
+      const movers = await dispatch(getGainers());
+      setMovers(movers);
+    };
+    parseMovers();
+  }, []);
 
-                    </div>
-                </div>
-            )
-
-        })
-    }
-
-    return (
-        <div>
-            {parsed[0]}
-            {parsed[1]}
-            {parsed[2]}
-            {parsed[3]}
-            {parsed[4]}
-        </div>
-    )
+  return (
+    <div>
+      <h3>Daily Movers</h3>
+      <div className="movers-div-for-cards">
+        {movers.slice(0, 5).map((ele) => (
+          <div class="movers-card">
+            <div class="movers-info">{ele.symbol}</div>
+            <div class="movers-info-change">$ {ele.change}</div>
+            <div class="movers-info-change">
+              % {ele.changesPercentage.toFixed(2)}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
