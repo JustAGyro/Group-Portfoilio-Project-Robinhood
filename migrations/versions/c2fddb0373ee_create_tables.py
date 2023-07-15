d31e8ff87955b2db31ed97655feeda352f0ee076
ahmad-shaukat <ahmadshaukat@yahoo.com>
2023-07-15 15:21:12 -0400

    added cascade to symbolist modal

--- migrations/versions/c2fddb0373ee_create_tables.py
+++ migrations/versions/c2fddb0373ee_create_tables.py
@@ -1,8 +1,8 @@
 """create tables
 
-Revision ID: b3f9e0afdc91
+Revision ID: c2fddb0373ee
 Revises: 
-Create Date: 2023-07-13 18:02:46.179907
+Create Date: 2023-07-15 13:52:14.520219
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'b3f9e0afdc91'
+revision = 'c2fddb0373ee'
 down_revision = None
 branch_labels = None
 depends_on = None
